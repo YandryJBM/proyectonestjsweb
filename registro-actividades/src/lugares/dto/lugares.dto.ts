d15b840c63b2a97b6ed62298d51556a520bc12a0
YandryJBM <121211394+YandryJBM@users.noreply.github.com>
2023-05-06 20:51:13 -0500

    Delete registro-actividades/src/lugares directory

--- registro-actividades/src/lugares/dto/lugares.dto.ts
+++
@@ -1,8 +0,0 @@
-//Creacion de clase donde se enviaran los datos de la aplicacion cliente/Servidor
-  export class CreateLugarDTO{
-    //atributos asignados por el docente.
-     pais:string;
-     ciudad:string;
-
-}
- 
